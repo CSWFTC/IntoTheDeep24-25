c9ba3c505c0de78602fe5dc13bd1a4dd160f638f
Rohit Karthik <rohitismaster@gmail.com>
2024-09-21 10:43:06 -0400

    test: update servo test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestServoContinuous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/TestServoContinuous.java
@@ -14,9 +14,9 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 @TeleOp(name = "Continuous Servo Test", group = "Test")
 public class TestServoContinuous extends  LinearOpMode {
     public static class Params {
-        public String servoName = "DroneServo";
+        public String servoName = "roti";
         public double servoForward = 1;
-        public double servoStartPos = 0d;
+//        public double servoStartPos = 0.5d;
     }
 
     public static Params PARAMS = new Params();
@@ -26,10 +26,9 @@ public class TestServoContinuous extends  LinearOpMode {
     private Servo servo;
 
     private boolean tlmServoForward = false;
-    private double tlmServoPosition = 0;
-    private double newPosition = 0;
+//    private double newPosition = 0;
 
-    private double turnValue = 0.6f;
+    private double turnValue = 0.5f;
 
 
     @Override
@@ -44,34 +43,39 @@ public class TestServoContinuous extends  LinearOpMode {
         tlmServoForward = (PARAMS.servoForward != 0);
         servo.setDirection(tlmServoForward ? Servo.Direction.FORWARD : Servo.Direction.REVERSE);
 
-        // Move Servo to initial Position
-        newPosition = PARAMS.servoStartPos;
-        servo.setPosition(newPosition);
-        tlmServoPosition = newPosition;
-
-
-
         while (opModeIsActive()) {
             update_telemetry();
-
-            // turnValue based conditional
-            if (this.turnValue > 0.5) {
-                // turns position based on the last setPosition command sent to servo
-                servo.setPosition(servo.getPosition()+10);
-            } else {
-                servo.setPosition(servo.getPosition()-10);
-            }
 
             GamePad.GameplayInputType inputType = gpInput.WaitForGamepadInput(100);
             switch (inputType) {
                 case BUTTON_A:
-                        this.turnValue = 0.6f;
+                    this.turnValue = 0.6f;
+                    updateServo();
                     break;
                 case BUTTON_Y:
                     this.turnValue = 0.4f;
+                    updateServo();
+                    break;
+                case DPAD_DOWN:
+                    if (this.turnValue >= 0.1f) {
+                        this.turnValue -= 0.1f;
+                    }
+                    updateServo();
+                    break;
+                case DPAD_UP:
+                    if (this.turnValue <= 0.9f) {
+                        this.turnValue += 0.1f;
+                    }
+                    updateServo();
                     break;
             }
         }
+    }
+
+    // update continuous servo based on turnValue
+    private void updateServo() {
+        servo.setPosition(this.turnValue);
+        update_telemetry();
     }
 
     public boolean initialize() {
@@ -83,7 +87,6 @@ public class TestServoContinuous extends  LinearOpMode {
         // Initialize Helpers
         try {
             servo = hardwareMap.servo.get(PARAMS.servoName);
-            // gamepad disabled
             gpInput = new GamePad(gamepad1);
             dashboard = FtcDashboard.getInstance();
             dashboard.clearTelemetry();
@@ -103,23 +106,19 @@ public class TestServoContinuous extends  LinearOpMode {
 
     private void update_telemetry() {
         telemetry.addLine("Servo Test");
-//        telemetry.addLine("Use Dpad to Set Position");
-//        telemetry.addLine("  Up/Down    +/- 0.1");
-//        telemetry.addLine("  Left/Right +/- 0.005");
-//        telemetry.addLine("Button A --> GoTo New Position");
-//        telemetry.addLine("Left Bumper --> Change Direction/n");
+        telemetry.addLine("Use Dpad to Change Rotational Speed");
+        telemetry.addLine("  Up/Down    +/- 0.1");
+        telemetry.addLine("Button A --> Spin Right");
+        telemetry.addLine("Button B --> Spin Left");
         telemetry.addLine().addData("Name     ", PARAMS.servoName );
         telemetry.addLine().addData("Direction", (tlmServoForward ? "Forward" : "Reverse") );
-        telemetry.addLine().addData("Curr Pos ", tlmServoPosition );
-        telemetry.addLine().addData("New Pos ", newPosition );
         telemetry.update();
 
         // FTC Dashboard Telemetry
         TelemetryPacket packet = new TelemetryPacket();
         packet.put("Name", PARAMS.servoName );
         packet.put("Direction", (tlmServoForward ? 1 : 0));
-        packet.put("Curr Position", tlmServoPosition);
-        packet.put("New Position", newPosition);
+        packet.put("TurnValue", this.turnValue);
         dashboard.sendTelemetryPacket(packet);
     }
 }
