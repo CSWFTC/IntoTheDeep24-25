8c074f6c95f8327557be975ee292b3bff801ae6f
anuthiru <149186696+anuthiru@users.noreply.github.com>
2024-09-28 11:41:11 -0400

    spline test auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestRoadRunner2.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.Tests;
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.roadrunner.Vector2d;
+
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
+public class TestRoadRunner2 {
+    @Config
+    @Autonomous
+    public class TestRoadRunner extends LinearOpMode {
+        //    private MecanumDrive mecanumDrive = new MecanumDrive();
+        private Pose2d beginPose = new Pose2d(0, 0, 0);
+
+
+        @Override
+        public void runOpMode() {
+            MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, beginPose);
+
+            waitForStart();
+
+            Actions.runBlocking(
+                    mecanumDrive.actionBuilder(
+                                    beginPose
+                            )
+                            .splineTo(new Vector2d(10, 0), Math.toRadians(180))
+                            .build()
+            );
+        }
+    }
+
+}
