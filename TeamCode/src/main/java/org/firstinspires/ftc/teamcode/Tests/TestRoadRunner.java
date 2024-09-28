03fc8ea2efaef93ae0bc2eddea379d2850cae4dd
Rohit Karthik <rohitismaster@gmail.com>
2024-09-28 11:35:30 -0400

    added roadrunner tests

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestRoadRunner.java
@@ -0,0 +1,33 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.Helper.DeferredActions;
+import org.firstinspires.ftc.teamcode.RoadRunner.MecanumDrive;
+
+@Config
+@Autonomous
+public class TestRoadRunner extends LinearOpMode {
+//    private MecanumDrive mecanumDrive = new MecanumDrive();
+    private Pose2d beginPose = new Pose2d(0, 0, 0);
+
+
+    @Override
+    public void runOpMode() {
+        MecanumDrive mecanumDrive = new MecanumDrive(hardwareMap, beginPose);
+
+        waitForStart();
+
+        Actions.runBlocking(
+                mecanumDrive.actionBuilder(
+                        beginPose
+                )
+                        .lineToX(100d)
+                        .build()
+        );
+    }
+}
