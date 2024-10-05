2043ed5cea3f90b11165ded52111eae170940284
Rohit Karthik <rohitismaster@gmail.com>
2024-10-05 12:47:04 -0400

    rr tests

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestRRNavigation.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestRRNavigation.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode.Tests;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Rotation2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
@@ -21,11 +24,22 @@ public class TestRRNavigation extends LinearOpMode {
         waitForStart();
 
         Actions.runBlocking(
-                mecanumDrive.actionBuilder(beginPose)
-                        .strafeTo(
-                                new Vector2d(50d, 0d)
-                        )
+            new SequentialAction(
+                    mecanumDrive.actionBuilder(beginPose)
+                            .lineToX(80d)
+                            .build(),
+                    mecanumDrive.actionBuilder(new Pose2d(80d, 0d, 0d))
+                        .turnTo(Math.toRadians(90d))
                         .build()
+            )
         );
+
+//        Actions.runBlocking(
+//                mecanumDrive.actionBuilder(beginPose)
+//                        .strafeTo(
+//                                new Vector2d(50d, 0d)
+//                        )
+//                        .build()
+//        );
     }
 }
