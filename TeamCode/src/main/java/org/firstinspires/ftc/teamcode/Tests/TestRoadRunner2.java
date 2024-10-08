f3044aff3748477aa172dff3a0e536746d44e125
Rohit Karthik <rohitismaster@gmail.com>
2024-10-08 19:04:06 -0400

    logging test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestRoadRunner2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestRoadRunner2.java
@@ -3,6 +3,7 @@ import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
@@ -43,88 +44,23 @@ public class TestRoadRunner2 extends LinearOpMode {
         waitForStart();
 
         Actions.runBlocking(
-                mecanumDrive.actionBuilder(
-                                this.position
-                        )
-                        .lineToX(110d)
-                        .build()
+              mecanumDrive.actionBuilder(this.position)
+                      .lineToX(100)
+                      .build()
         );
 
-        try {
-            Thread.sleep(1000); // Wait for 1 second
-        } catch (InterruptedException e) {
-            // Handle the interrupted exception if necessary
-            telemetry.addData("Error", "Thread interrupted!");
-            telemetry.update();
-        }
+        telemetry.addLine(mecanumDrive.pose.heading.toString());
+        telemetry.update();
 
-        changePosition(110d, getY(), getHeading());
-
-        Actions.runBlocking(
-                mecanumDrive.actionBuilder(
-                                new Pose2d(30, 0, 0)
-                )
-                        .splineTo(new Vector2d(0d, -30d),Math.toRadians(180))
-                        .build()
-        );
-
-        try {
-            Thread.sleep(1000); // Wait for 1 second
-        } catch (InterruptedException e) {
-            // Handle the interrupted exception if necessary
-            telemetry.addData("Error", "Thread interrupted!");
-            telemetry.update();
-        }
-
-        changePosition(20d, 30d, Math.toRadians(180));
+        changePosition(100, getY(), getHeading());
 
         Actions.runBlocking(
                 mecanumDrive.actionBuilder(this.position)
-                        .turnTo(270d)
+                        .turnTo(180)
                         .build()
         );
 
-        try {
-            Thread.sleep(1000); // Wait for 1 second
-        } catch (InterruptedException e) {
-            // Handle the interrupted exception if necessary
-            telemetry.addData("Error", "Thread interrupted!");
-            telemetry.update();
-        }
-
-
-        changePosition(getX(), getY(), Math.toRadians(270d));
-
-
-        Actions.runBlocking(
-                mecanumDrive.actionBuilder(this.position)
-                        .strafeTo(new Vector2d(-35, -20))
-                        .turnTo(300)
-                        .build()
-        );
-
-//        Actions.runBlocking(
-//                mecanumDrive.actionBuilder(this.position)
-//                        .splineTo(new Vector2d(10d, 30d), Math.toRadians(270d))
-//                        .build()
-//        );
-
-        try {
-            Thread.sleep(1000); // Wait for 1 second
-        } catch (InterruptedException e) {
-            // Handle the interrupted exception if necessary
-            telemetry.addData("Error", "Thread interrupted!");
-            telemetry.update();
-        }
-
-        changePosition(10d, 20d, Math.toRadians(270));
-//
-//        Actions.runBlocking(
-//                mecanumDrive.actionBuilder(this.position)
-//                        .lineToY(30d)
-//                        .build()
-//        );
-
-//        changePosition(getX(), 50d, getHeading());
+        telemetry.addLine(mecanumDrive.pose.heading.toString());
+        telemetry.update();
     }
 }
