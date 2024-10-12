841c751b42251ffc62164f60495aa509bcc8675d
asvika425 <148814963+asvika425@users.noreply.github.com>
2024-10-12 11:00:56 -0400

    LEDLightsYAY

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LEDLights.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.LED;
+
+/*This OpMode assumes that the REV Digital Indicator is setup as 2 Digital Channels named
+ * front_led_green and front_led_red. (the green should be the lower of the 2 channels it is plugged
+ into and the red should be the higher)
+*/
+
+@TeleOp(name = "Concept: RevLED", group = "Concept")
+
+public class LEDLights extends OpMode {
+    LED frontLED_red;
+    LED frontLED_green;
+
+    public void init() {
+        frontLED_green = hardwareMap.get(LED.class, "front_led_green");
+        frontLED_red = hardwareMap.get(LED.class, "front_led_red");
+    }
+
+    public void loop() {
+        if (gamepad1.a) {
+            frontLED_red.on();
+        } else {
+            frontLED_red.off();
+        }
+        if (gamepad1.b) {
+            frontLED_green.on();
+        } else {
+            frontLED_green.off();
+        }
+    }
+}
