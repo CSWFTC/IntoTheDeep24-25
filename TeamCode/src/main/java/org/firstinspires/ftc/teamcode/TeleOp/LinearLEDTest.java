0ee01b81ec3217cc3bf2d450af24d0ce972f4b69
Rohit Karthik <rohitismaster@gmail.com>
2024-10-16 13:09:02 -0400

    added TelemetryEvent class which handles Telemetry using EventBus

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/LinearLEDTest.java
@@ -0,0 +1,60 @@
+/*
+This file is an LED TEST FILE - Which is used to test the TelemetryEvent class
+ */
+
+
+package org.firstinspires.ftc.teamcode.TeleOp;
+
+import com.acmerobotics.roadrunner.Line;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.LED;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.Helper.EventBus.EventBus;
+import org.firstinspires.ftc.teamcode.Helper.Telemetry.TelemetryEvent;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@TeleOp(name = "Concept: RevLED", group = "Concept")
+
+public class LinearLEDTest extends LinearOpMode {
+    LED frontLED_red;
+    LED frontLED_green;
+    EventBus eventBus = EventBus.getInstance();
+    Set<String> targets = new HashSet<>();
+
+    public void runOpMode() {
+        // Testing TelemetryEvent
+        TelemetryEvent __ = TelemetryEvent.getInstance(); // sets up listener
+
+        this.targets.add("push_telemetry");
+
+        frontLED_green = hardwareMap.get(LED.class, "front_led_green");
+        frontLED_red = hardwareMap.get(LED.class, "front_led_red");
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+            if (gamepad1.a) {
+                frontLED_red.on();
+                this.eventBus.emit(this.targets, telemetry, "Red Light Status \n ON");
+
+            } else {
+                frontLED_red.off();
+                this.eventBus.emit(this.targets, telemetry, "Red Light Status \n OFF");
+            }
+            if (gamepad1.b) {
+                frontLED_green.on();
+                this.eventBus.emit(this.targets, telemetry, "Green Light Status \n ON");
+            } else {
+                frontLED_green.off();
+                this.eventBus.emit(this.targets, telemetry, "Green Light Status \n OFF");
+            }
+        }
+    }
+}
