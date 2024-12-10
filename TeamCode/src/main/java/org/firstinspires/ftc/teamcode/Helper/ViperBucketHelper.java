0597a099640de5fa524ea49a8a02b7f023677174
jaltrichter <jaltrichter@outlook.com>
2024-12-10 18:50:17 -0500

    Robot Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/ViperBucketHelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/ViperBucketHelper.java
@@ -8,16 +8,26 @@ import org.firstinspires.ftc.teamcode.Helper.ViperSlideActions.ViperAction;
 import org.firstinspires.ftc.teamcode.Helper.ViperSlideActions.ViperSlideHelper;
 
 public class ViperBucketHelper  {
-
     private HardwareMap hardwareMap;
-
     private ViperSlideHelper viperSlideHelper;
     private BucketAction bucketAction;
     private ViperAction viperAction;
 
     public static class Params {
+        public double bucketStartPos = 0.11;  // Tucked in
+        public double bucketCatchPos = 0.085;  // Catch from Beak
+        public double bucketDumpPos = 0.01;    // Dump to Basket
+
         public int viperStartPos = 0;
-        public double bucketStartPos = 0.11;
+
+        public boolean viperMotorReverse = true;
+        public double viperHighBasketPos = 3150;  // High Basket
+        public double viperLowBasketPos = 1220;  // Low Basket (Approx 38% of High Basket)
+        public double viperCatchPoint = 0;       // Catch Point for Sample
+        public double viperMotorSpeed = 0.9;
+
+        public double dumpLowBasketDelay = 750;  //ms To Wait for Dump
+        public double dumpHighBasketDelay = 1250;  //ms To Wait for Dump
     }
 
     public static Params PARAMS = new Params();
