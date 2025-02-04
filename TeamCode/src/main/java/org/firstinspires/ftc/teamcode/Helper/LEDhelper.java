266514757a0eb3ff3a3aa4b8645e02e707f46c33
anuthiru <149186696+anuthiru@users.noreply.github.com>
2025-02-04 17:30:15 -0500

    LED helper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/LEDhelper.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/LEDhelper.java
@@ -1,1 +1,24 @@
+package org.firstinspires.ftc.teamcode.Helper;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+public class LEDhelper {
+    private Servo servo;
+    private double position;
+
+    public LEDhelper(HardwareMap hardwareMap, String servoName, double startPos) {
+        servo = hardwareMap.get(Servo.class, servoName);
+        position = startPos;
+        servo.setPosition(position);
+    }
+
+    public void setPosition(double newPosition) {
+        position = newPosition;
+        servo.setPosition(position);
+    }
+
+    public double getPosition() {
+        return position;
+    }
+}
