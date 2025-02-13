b781330c778ebfd3efab52c62e519fe39b909f45
jaltrichter <jaltrichter@outlook.com>
2025-02-13 11:51:01 -0500

    Robot Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Hardware/TestThreeMotorHang.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp/Hardware/TestThreeMotorHang.java
@@ -11,8 +11,8 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.Helper.GamePad;
 
 @Config
-@TeleOp(name="Test Two Motors", group="Hardware")
-public class TestTwoMotors extends LinearOpMode {
+@TeleOp(name="Test 3 Motor Hang", group="Hardware")
+public class TestThreeMotorHang extends LinearOpMode {
 
     public static final String version = "1.0 ";
 
@@ -20,7 +20,9 @@ public class TestTwoMotors extends LinearOpMode {
         public String motor1Name = "hookLeft";
         public Boolean motor1Forward = false;
         public String motor2Name = "hookRight";
-        public Boolean motor2Forward = false;
+        public Boolean motor2Forward = true;
+        public String motor3Name = "hookSecond";
+        public Boolean motor3Forward = false;
     }
 
     public static Params PARAMS = new Params();
@@ -29,7 +31,7 @@ public class TestTwoMotors extends LinearOpMode {
     @Override
     public void runOpMode() {
         telemetry.setDisplayFormat(Telemetry.DisplayFormat.MONOSPACE);
-        telemetry.addLine("Two Motor Test");
+        telemetry.addLine("Three Motor Test");
         telemetry.addData("Version Number", version);
         telemetry.addLine();
         telemetry.addData(">", "Press Start to Launch");
@@ -38,6 +40,7 @@ public class TestTwoMotors extends LinearOpMode {
 
         DcMotor motor1 = hardwareMap.dcMotor.get(PARAMS.motor1Name);
         DcMotor motor2 = hardwareMap.dcMotor.get(PARAMS.motor2Name);
+        DcMotor motor3 = hardwareMap.dcMotor.get(PARAMS.motor3Name);
 
         motor1.setDirection((PARAMS.motor1Forward) ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE);
         motor1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -48,6 +51,11 @@ public class TestTwoMotors extends LinearOpMode {
         motor2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         motor2.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        motor3.setDirection((PARAMS.motor3Forward) ? DcMotorSimple.Direction.FORWARD : DcMotorSimple.Direction.REVERSE);
+        motor3.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         GamePad gpIn1 = new GamePad(gamepad1);
 
@@ -66,6 +74,16 @@ public class TestTwoMotors extends LinearOpMode {
                     }
                     motor1.setPower(gamepad1.left_stick_y);
                     motor2.setPower(gamepad1.left_stick_y);
+                    break;
+
+                case LEFT_TRIGGER:
+                    motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    motor3.setPower(-gamepad1.left_trigger);
+                    break;
+
+                case RIGHT_TRIGGER:
+                    motor3.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+                    motor3.setPower(gamepad1.right_trigger);
                     break;
 
                 case BUTTON_A:
@@ -93,6 +111,7 @@ public class TestTwoMotors extends LinearOpMode {
                 case BUTTON_R_BUMPER:
                     motor1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                     motor2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                    motor3.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
                     break;
 
                 case BUTTON_L_BUMPER:
@@ -106,6 +125,9 @@ public class TestTwoMotors extends LinearOpMode {
             telemetry.addData("M2 Pos", motor2.getCurrentPosition());
             telemetry.addData("M2 Power", motor2.getPower());
             telemetry.addData("M2 Direction", (motor2.getDirection() == DcMotorSimple.Direction.FORWARD) ? "Forward" : "Reverse");
+            telemetry.addData("M3 Pos", motor3.getCurrentPosition());
+            telemetry.addData("M3 Power", motor3.getPower());
+            telemetry.addData("M3 Direction", (motor3.getDirection() == DcMotorSimple.Direction.FORWARD) ? "Forward" : "Reverse");
             telemetry.update();
         }
     }
