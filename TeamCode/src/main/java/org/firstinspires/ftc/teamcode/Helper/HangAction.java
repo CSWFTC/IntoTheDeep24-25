bc9f7706786822b9470422c6a8e056371f1bfa71
anuthiru <149186696+anuthiru@users.noreply.github.com>
2025-02-14 10:31:36 -0500

    hang flip

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/HangAction.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/HangAction.java
@@ -3,17 +3,19 @@ package org.firstinspires.ftc.teamcode.Helper;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class HangAction {
     private final DcMotor left;
     private final DcMotor right;
     private final DcMotor hang2;
-
+    private final Servo grapple;
 
     public HangAction(HardwareMap hardwareMap) {
         left = hardwareMap.get(DcMotor.class, "hookLeft");
         right = hardwareMap.get(DcMotor.class, "hookRight");
         hang2 = hardwareMap.get(DcMotor.class, "hookSecond");
+        grapple = hardwareMap.get(Servo.class, "hookServo");
 
         left.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         right.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -23,30 +25,36 @@ public class HangAction {
         right.setDirection(DcMotorSimple.Direction.REVERSE);
         hang2.setDirection(DcMotorSimple.Direction.REVERSE);
 
-
         left.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         right.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         hang2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
 
-
+        grapple.setPosition(0.0);
     }
-
 
     public void moveMotors(double power) {
         left.setPower(power);
         right.setPower(power);
     }
 
-    public void moveHang2(double power){
+    public void moveHang2(double power) {
         hang2.setPower(power);
     }
 
-    public void moveHangDown(double power){
+    public void moveHangDown(double power) {
         hang2.setPower(-power);
     }
 
+    public void flipUp() {
+        grapple.setPosition(1.0);
+    }
 
+    public void flipDown() {
+        grapple.setPosition(0.0);
+    }
 }
+
+
 
 
 
