8abdfe6ce0cd87cbd2200e51c45377d4d384ed9d
asvika425 <148814963+asvika425@users.noreply.github.com>
2025-02-19 18:07:14 -0500

    auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Beak/newBeak.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Helper/Beak/newBeak.java
@@ -238,7 +238,7 @@ public class newBeak {
 
     public Action dropToHuman (){
         return packet -> {
-            SystemClock.sleep(1500);
+            SystemClock.sleep(1000);
             openBeak();
             ElbStart();
             return false;
