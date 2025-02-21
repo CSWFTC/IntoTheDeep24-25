f65b54125a14e8332441eeae0d80a414ceefaa23
anuthiru <149186696+anuthiru@users.noreply.github.com>
2025-02-21 15:05:59 -0500

    auton actions test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoTestFun.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoTestFun.java
@@ -1,83 +1,50 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
-import com.acmerobotics.roadrunner.SequentialAction;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.acmerobotics.roadrunner.ftc.Actions;
-
 import org.firstinspires.ftc.teamcode.Helper.Beak.newBeak;
 import org.firstinspires.ftc.teamcode.Helper.ViperSlide.BucketAction;
 import org.firstinspires.ftc.teamcode.Helper.ViperSlide.ClawAction;
 import org.firstinspires.ftc.teamcode.Helper.ViperSlide.ViperAction;
 
-@Autonomous(name = "AutoTestFun", group = "RoadRunner")
+@Autonomous(name = "AutoTestFun", group = "Test")
 public class AutoTestFun extends LinearOpMode {
 
-    public static class Params {
-        public boolean easy = false;
-    }
-
-    public static Params PARAMS = new Params();
     private ClawAction Claw;
     private ViperAction Viper;
     private newBeak Beak;
     private BucketAction Bucket;
-    // paw --> beak
-    // roar --> claw
 
-    public void runOpMode(){
+    public void runOpMode() {
         Claw = new ClawAction(hardwareMap);
         Viper = new ViperAction(hardwareMap);
         Beak = new newBeak(hardwareMap);
         Bucket = new BucketAction(hardwareMap);
 
-        Claw.CloseGrip();
-        Beak.autonStartPos();
-
         waitForStart();
 
-        telemetry.addData("okay", "so code needs to push 7");
+        telemetry.addLine("Starting Test Sequence");
         telemetry.update();
 
-        if(PARAMS.easy){
-            forward();
-        }
-        else{
-            Grabbing1(); //
-            Grabbing2();
-            Grabbing3();
-        }
-    }
+        // Pick up sample
+        Claw.grabFromHuman();
+        sleep(500);
+        Beak.autonReachOB();
+        sleep(500);
 
-    public void Grabbing1(){
+        // Move sample down
+        Viper.perfBeforeDropOff();
+        sleep(500);
+        Bucket.autonBucketDown();
+        sleep(500);
 
-        Actions.runBlocking(new SequentialAction(
-                Viper.perfBeforeDropOff(),
-                Beak.autonReachOB()
-        ));
-    }
+        // Drop off at human player zone
+        Beak.autonDropToHuman();
+        sleep(500);
+        Claw.placeOnSub();
+        sleep(500);
 
-    public void Grabbing2(){
-
-        Actions.runBlocking(new SequentialAction(
-                Beak.grabAndDrop(),          // In-place action (grab and drop)
-                Viper.clawHumanGrab()       // In-place action (grab from human)
-        ));
-    }
-
-    public void Grabbing3(){
-
-        Actions.runBlocking(new SequentialAction(
-                Viper.clawHumanGrab(),
-                Bucket.autonBucketDown()
-        ));
-    }
-
-    private void forward(){
-
-        Actions.runBlocking(new SequentialAction(
-                Viper.perfBeforeDropOff(),
-                Bucket.autonBucketDown()
-        ));
+        telemetry.addLine("Test Sequence Complete");
+        telemetry.update();
     }
 }
