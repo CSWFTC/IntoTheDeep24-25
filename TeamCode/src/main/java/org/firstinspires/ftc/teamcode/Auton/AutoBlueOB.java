46e3e9e7f8f53c5ffad3621f8cbcb5a2150cd1b6
asvika425 <148814963+asvika425@users.noreply.github.com>
2025-02-21 23:04:26 -0500

    auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auton/AutoBlueOB.java
@@ -117,7 +117,7 @@ public class AutoBlueOB extends LinearOpMode {
         Action Turning = drive.actionBuilder(drive.pose)
                 .setReversed(true)
                 .turnTo(Math.toRadians(35))
-                .splineToConstantHeading(new Vector2d(-19, 31), Math.toRadians(35))
+                .splineToConstantHeading(new Vector2d(-18, 32), Math.toRadians(35))
                 .build();
         Actions.runBlocking(new SequentialAction(Turning, Beak.autonDropSampleToHuman()));
     }
